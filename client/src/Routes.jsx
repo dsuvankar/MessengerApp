8ae9920bcc45f69c5b213dd9bbfe72155a3a3153
dsuvankar <98757134+dsuvankar@users.noreply.github.com>
2024-01-08 14:05:32 +0530

    Update Routes.jsx

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import RegisterAndLogin from "./RegisterAndLogin";
 import { UserContext } from "./UserContext";
-import Chat from "./chat";
+import Chat from "./Chat.jsx";
 
 export default function Routes() {
   const { username, id } = useContext(UserContext);
